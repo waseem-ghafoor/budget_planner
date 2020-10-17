dad2dcf950b0779e6cbb85b1391e358788a79cd4
waseem <waseem.ghafoor@clustox.com>
2020-10-17 08:30:31 +0500

    add functionality to change email and password for admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,20 +1,6 @@
 ActiveAdmin.register User do
-permit_params :email, user_access_attributes: [:id, :plan]
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :_id, :email, :encrypted_password, :sign_in_count, :deleted_at, :phone_verified
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:_id, :email, :encrypted_password, :sign_in_count, :deleted_at, :phone_verified]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+permit_params :email, :password, user_access_attributes: [:id, :plan]
+  
   index do
     selectable_column
     id_column
@@ -28,24 +14,40 @@ permit_params :email, user_access_attributes: [:id, :plan]
     column 'subscription_id' do |user|
       user.user_access.paypal_subscription_id
     end
-
     actions
   end
 
   form do |f|
     f.inputs "User Details" do
-      f.input :email, input_html: { disabled: true } 
+      f.input :email 
       f.inputs do
-      f.fields_for :user_access  do |j|
-        j.input :plan, as: :select, collection: [ ['Free Plan', :free], ['Plus Plan', :plus], ['Prime Plan', :prime]]
+        f.fields_for :user_access  do |j|
+          j.input :plan, as: :select, collection: [ ['Free Plan', :free], ['Plus Plan', :plus], ['Prime Plan', :prime]]
+        end
+      f.input :password
       end
-    end
-      
-      #f.input :subscription
-
+     
     end
     f.actions
   end
-    
+  
+  controller do
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
+      if params[:user][:email].blank?
+        params[:user].delete("email")
+        params[:user].delete("email_confirmation")
+      end
+      super
+    end
+  end
+  
+  before_save do |user|
+    user.skip_confirmation!
+    user.skip_reconfirmation!
+  end  
   
 end
